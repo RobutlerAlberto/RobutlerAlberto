e27ee1007394bd27ad3fb0874d96ff9dee536c75
rospuye <isabel.rosario@ua.pt>
2023-02-03 16:26:31 +0000

    lots more stuff

--- alberto_navigation/src/alberto_search.py
+++ alberto_navigation/src/alberto_search.py
@@ -4,7 +4,44 @@ import math
 import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import PoseWithCovarianceStamped, Point32
+from time import sleep
 
+
+"""
+entities present:
+- coords_listener -> listens for current coordinates of the robot
+- goal_listener -> listens for event indicating that the robot has reached its current goal
+- goal_publisher -> publishes new robot goal
+
+search flow:
+(1) a new mission pops up: find object A
+(2) coords_listener listens to find out current position
+(3) search function figures out optimal trajectory depending on current position
+(4) this returns an ordered list of locations
+(5) goal_publisher fetches coordinates of first location and publishes them, triggering the robot to go
+(6) goal_listener listens for goal reaching event
+(7) when goal has been reached, turn 360º and check if the object is in sight
+(8) if so, return
+(9) if not, repeat steps (5)-(8) for next location on the list
+
+possible robot states, in order: (coords_listener / goal_listener / goal_publisher)
+- dormant: the search class has been initialized but there is no mission currently going on
+    - off / on / off
+- ready_for_path: based on the robot's current coordinates, we want to compute the optimal search path
+    - off / off / off
+    - here we call search function and save list of locations #!
+- defining_path: computing the optimal search path
+    - off / off / off
+- defining_goal: we want to announce the next goal location
+    - off / off / on
+- travelling: we want to listen for when we have reached the goal location
+    - off / on / off
+- ready_for_turn: we want to turn 360º and look for object
+    - off / off / off
+    - call turn function -> this function changes the robot's state according to rules (8) and (9) #!
+- turning: we are turning 360º and looking for object
+    - off / off / off
+"""
 
 
 class Search():
@@ -57,19 +94,52 @@ class Search():
                 'terraceB': (-7.15, -3.65)
             }
         }
-        self.searched_rooms = []
+        # self.searched_rooms = []
+        self.search_path = None
+        
+        rospy.init_node('search_pubsub', anonymous=True)
         self.current_coords = None
-        self.coords_listener = self.init_listener()
-        self.coords_publisher = self.init_publisher()
-    
-    def reset(self):
-        self.searched_rooms = []
+        self.goal_reached = False
+        self.goal_object = None
 
-    def find_closest_room(self, coords):
+        self.coords_listener = rospy.Subscriber('amcl_pose', PoseWithCovarianceStamped, self.coords_listener_callback)
+        self.goal_listener = None #! TODO
+        self.mission_listener = None #! TODO
+        self.goal_publisher = rospy.Publisher('/goal_coords', Point32, queue_size=10)
+
+        self.state = 'dormant'
+        self.run()
+
+    def run(self):
+        while True:
+            # if self.state == 'dormant':
+            #     pass
+            if self.state == 'ready_for_path':
+                self.find_optimal_search_path()
+            elif self.state == 'ready_for_next_stop':
+                next_stop = self.search_path.pop(0)
+                next_stop_coords = self.house_rooms['coordinates'][next_stop]
+                self.go(next_stop_coords)
+            
+            # elif self.state == 'defining_path':
+            #     pass
+            # elif self.state == 'travelling':
+            #     pass
+            
+            elif self.state == 'ready_for_turn':
+                # call turn()
+                pass
+            elif self.state == 'turning':
+                pass
+    
+    # def reset(self):
+    #     self.searched_rooms = []
+
+    def find_closest_room(self, coords, searched_rooms):
         x1, y1 = coords
         min_d, closest_room = float('inf'), None
         for room in self.house_rooms['coordinates']:
-            if room not in self.searched_rooms:
+            if room not in searched_rooms:
                 x2, y2 = self.house_rooms['coordinates'][room]
                 d = math.sqrt((x2[0]-x1[0])**2 + (y2[1]-y1[1])**2)
                 if d < min_d:
@@ -78,56 +148,85 @@ class Search():
         return closest_room
     
     def go(self, coords): # publish to /goal_coords (geometry_msgs.msg Point32)
+        self.state = 'travelling'
         coords_msg = Point32(x=coords[0], y=coords[1], z=0)
         self.coords_publisher.publish(coords_msg)
-        # coords_msg.x = coords[0]
-        # coords_msg.y = coords[1]
-        # coords_msg.z = 0
 
     def turn(self):
+        self.state = 'turning'
+        # if object found, do 'self.goal_reached = True' before returning
         return False # returns True if desired object is found
 
-    def search(self):
-        if self.current_coords:
-            start_room = self.find_closest_room(self.current_coords)
-            start_room_coords = self.house_rooms['coordinates'][start_room]
-            self.go(start_room_coords)
-            found = self.turn()
-            self.searched_rooms.append(start_room)
+    def find_optimal_search_path(self):
+        if self.current_coords: # if the robot doesn't know its own position yet, this function will be continuously called until it does
+            self.state = 'defining_path' # this way, find_optimal_search_path is only called once per mission
+            path = []
+            starting_coords = self.current_coords
 
-            # while there are still rooms to search
-            while not found and len(self.searched_rooms) < len(self.house_rooms['coordinates']):
-                next_room = self.find_closest_room(start_room_coords, self.searched_rooms)
-                if next_room:
-                    next_room_coords = self.house_rooms['coordinates'][next_room]
-                    self.go(next_room_coords)
-                    found = self.turn()
-                    self.searched_rooms.append(next_room)
-                else:
-                    break
+            while len(path) < len(self.house_rooms['coordinates']):
+                new_room = self.find_closest_room(starting_coords, path)
+                path.append(new_room)
+                new_room_coords = self.house_rooms['coordinates'][new_room]
+                starting_coords = new_room_coords
+
+            self.search_path = path
         else:
-            print('Robot doesn\'t know its own position yet, wait a bit!')
+            #! don't know if this works but the intent was to have a 2 second delay between each call to this function
+            #! needs testing
+            sleep(2)
 
-    def init_listener(self):
-        rospy.init_node('coords_listener', anonymous=True)
-        rospy.Subscriber('amcl_pose', PoseWithCovarianceStamped, self.listener_callback)
-        rospy.spin()
 
-    def listener_callback(self, data):
+            # start_room = self.find_closest_room(self.current_coords)
+            # start_room_coords = self.house_rooms['coordinates'][start_room]
+            # self.go(start_room_coords)
+            # found = self.turn()
+            # self.searched_rooms.append(start_room)
+
+            # # while there are still rooms to search
+            # while not found and len(self.searched_rooms) < len(self.house_rooms['coordinates']):
+            #     next_room = self.find_closest_room(start_room_coords, self.searched_rooms)
+            #     if next_room:
+            #         next_room_coords = self.house_rooms['coordinates'][next_room]
+            #         self.go(next_room_coords)
+            #         found = self.turn()
+            #         self.searched_rooms.append(next_room)
+            #     else:
+            #         break
+
+    # def init_listener(self):
+    #     rospy.loginfo('INIT SUBSCRIBER !!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+    #     sub = rospy.Subscriber('amcl_pose', PoseWithCovarianceStamped, self.listener_callback)
+    #     return sub
+
+    def coords_listener_callback(self, data):
         position = data.pose.pose.position
         self.current_coords = (position.x, position.y)
-        rospy.loginfo(self.__str__())
+        # rospy.loginfo('CALLBACK CALLBACK CALLBACK')
+        # rospy.loginfo(self.__str__())
 
-    def init_publisher(self):
-        rospy.loginfo('INIT PUBLISHER !!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-        rospy.init_node('coords_publisher', anonymous=True)
-        pub = rospy.Publisher('/goal_coords', Point32, queue_size=10)
-        return pub
+    def goal_listener_callback(self):
+        self.goal_reached = True
+        if len(self.search_path == 0):
+            self.state = '' #! TODO
+        else:
+            self.state = 'ready_for_next_stop'
+
+    def mission_listener_callback(self, data):
+        #! TODO: setup mission from data
+        self.goal_reached = False
+        # self.goal_object = ...  
+        self.state = 'ready_for_path'
+
+    # def init_publisher(self):
+    #     rospy.loginfo('INIT PUBLISHER !!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+    #     pub = rospy.Publisher('/goal_coords', Point32, queue_size=10)
+    #     return pub
 
     def __str__(self):
         return "robot position: " + str(self.current_coords)
 
 search = Search()
+# search.go((1,1))
 
 
 
